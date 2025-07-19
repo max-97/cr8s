172290e204b72de39b9933db9b1939e4e601ef01
Max Braun <13639830+max-97@users.noreply.github.com>
2025-07-19 20:55:35 +0200

    Fix typo in crates migrations file

--- migrations/2025-07-16-191832_create_crates/up.sql
+++ migrations/2025-07-16-191832_create_crates/up.sql
@@ -3,7 +3,7 @@ create table crates (
     rustacean_id integer not null references rustaceans(id),
     code varchar(64) not null,
     name varchar(128) not null,
-    versio varchar(64) not null,
+    version varchar(64) not null,
     description text,
     created_at timestamp default now() not null
 );
